a463d58a48be4f029c26163255dc6ea67686d3c6
egafrandika <ega.frandika@fintopia.tech>
2023-05-19 11:14:49 +0700

    fucntion download

--- lib/pages/ImagePage.dart
+++ lib/pages/ImagePage.dart
@@ -1,11 +1,40 @@
+import 'dart:io';
 import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 class ImagePage extends StatelessWidget {
   final String tag;
   final String imageUrl;
 
   ImagePage({required this.tag, required this.imageUrl});
+
+  Future<void> _downloadImage(BuildContext context) async {
+    final directory = await getExternalStorageDirectory();
+    String imagePath;
+
+    if (imageUrl.contains('firstTab')) {
+      imagePath = '${directory!.path}/$tag.jpg';
+    } else if (imageUrl.contains('secondTab')) {
+      imagePath = '${directory!.path}/$tag.jpeg';
+    } else {
+      imagePath = '';
+    }
+
+    final downloadUrl = 'https://example.com/images/secondTab/$tag.jpeg'; // Replace with the actual URL of the image
+
+    final response = await http.get(Uri.parse(downloadUrl));
+    final bytes = response.bodyBytes;
+
+    await File(imagePath).writeAsBytes(bytes);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Gambar Diunduh'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +46,6 @@ class ImagePage extends StatelessWidget {
   } else {
     imagePath = '';
   }
-
     return Scaffold(
       extendBody: true,
       body: DismissiblePage(
@@ -52,11 +80,14 @@ class ImagePage extends StatelessWidget {
                   ),
                   Padding(
                     padding: EdgeInsets.all(10),
-                    child: Icon(
-                      Icons.download,
-                      color: Colors.white,
-                      size: 40,
-                    ),
+                    child: GestureDetector(
+                      onTap: () => _downloadImage(context),
+                      child: Icon(
+                        Icons.download,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    )
                   ),
                   Padding(
                     padding: EdgeInsets.all(10),
